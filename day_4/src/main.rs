1547fe5ffa576529df80a3f80d21c9a5c67ebb67
MiikaM <miika.mikkonen5@gmail.com>
2022-12-04 11:16:42 +0200

    Read input

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    let result = fs::read_to_string("./puzzle/input.txt");
+    let contents = match result {
+        Ok(message) => message,
+        Err(error) => match error.kind() {
+            io::ErrorKind::NotFound => {
+                println!("File was not found: {error:?}");
+                String::from("File was not found")
+            }
+            _ => panic!("There was an error reading the file: {error:?}"),
+        },
+    };
 }
