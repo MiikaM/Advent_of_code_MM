5bc2f235da41334fc6013f572174617cfdbce412
MiikaM <miika.mikkonen5@gmail.com>
2022-12-04 11:29:25 +0200

    Read input

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,7 +1,10 @@
-use std::{fs, io};
+use std::{env, fs, io};
 
 fn main() {
-    let result = fs::read_to_string("./puzzle/input.txt");
+    let args: Vec<String> = env::args().collect();
+    // let file_path = &args[1];
+    let file_path = String::from("./puzzle/input.txt");
+    let result = fs::read_to_string(file_path);
     let contents = match result {
         Ok(message) => message,
         Err(error) => match error.kind() {
@@ -12,4 +15,8 @@ fn main() {
             _ => panic!("There was an error reading the file: {error:?}"),
         },
     };
+
+    let list_of_pairs: Vec<&str> = contents.split("\r\n").collect();
+
+    println!("{list_of_pairs:?}")
 }
