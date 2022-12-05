19265fa6120c9dacb9f8bb6ea32592860de68b5a
MiikaM <miika.mikkonen5@gmail.com>
2022-12-05 11:42:17 +0200

    Parsing table data, not done

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,9 +1,10 @@
-use std::{time::Instant, env, fs, io};
+use std::{env, fs, io, time::Instant, vec};
 
 fn main() {
     let now = Instant::now();
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+    let _args: Vec<String> = env::args().collect();
+    // let file_path = &args[1];
+    let file_path = "./puzzle/input.txt";
     let result = fs::read_to_string(file_path);
     let contents = match result {
         Ok(message) => message,
@@ -14,13 +15,46 @@ fn main() {
             _ => panic!("There was an error reading the file: {error:?}"),
         },
     };
-
     // do something with the contents
-
+    let parsed_input = parse_input(&contents);
     println!(
         "Time it took to run: {} seconds",
         now.elapsed().as_secs_f64()
     );
-    
+
     println!("Result");
+}
+
+fn parse_input<'a>(input: &'a String) -> (Vec<&'a str>, Vec<&'a str>) {
+    let (crates, instructions): (&str, &str) = input.split_once("\r\n\r\n").unwrap();
+
+    let crate_lines: Vec<&str> = crates.split("\r\n").collect();
+    let instruction_lines: Vec<&str> = instructions.split("\r\n").collect();
+
+    for line in &crate_lines {
+        let stack_numbers: Vec<&str> = line.split("").collect();
+        println!("Length of line: {} and the items: {:?}", stack_numbers.len(), stack_numbers);
+
+        
+    }
+
+    println!("{}", crate_lines.last().unwrap().len());
+
+    let stack_line = crate_lines.last().unwrap();
+
+    let stack_numbers: Vec<&str> = stack_line.split(" ").collect();
+    let amount_of_stacks = stack_numbers[stack_numbers.len() - 2].parse::<u8>().unwrap_or(0);
+    let mut stacks:Vec<Vec<&str>> = vec![vec![]; amount_of_stacks.into()];
+
+    
+    stacks[0].push("test");
+    
+
+    println!("{stacks:?}" );
+
+    (instruction_lines, crate_lines)
+}
+
+fn move_crates() -> () {
+    println!("Move crates")
 }
---
+++ day_5/target/debug/deps/day_5.d
@@ -0,0 +1,5 @@
+C:\Users\miika\Code\version-control\Advent_of_code\day_5\target\debug\deps\day_5.exe: src\main.rs
+
+C:\Users\miika\Code\version-control\Advent_of_code\day_5\target\debug\deps\day_5.d: src\main.rs
+
+src\main.rs:
